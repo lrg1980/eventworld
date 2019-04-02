9538fc8e34fa4905b7847654f0f9b6af454bdf16
lrg1980 <admin@binweb.net>
2019-04-02 11:16:55 -0300

    Sumamos categorias desde API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
+import Formulario from './componentes/Formulario';
 
 class App extends Component {
+
+  token = '3OS7IYV3H7YNCTBU5GLJ';
+
+  state = {
+    categorias: []
+  }
+  componentDidMount(){
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias = async () => {
+
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+    await fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(categorias => {
+        this.setState({
+          categorias: categorias.categories
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
+        <div className="uk-container">
+          <Formulario
+            categorias={this.state.categorias}/>
+        </div>
+
       </div>
     );
   }
